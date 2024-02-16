3e3958036f39f04fb4844e2c85f2fb9dec736e11
Jérémie Nuel <jeremie.nuel@pm.me>
2024-02-16 17:39:18 +0100

    template

---
+++ README.md
@@ -0,0 +1,3 @@
+composer install
+
+php -S localhost:8080 -t public
--- public/index.php
+++ public/index.php
@@ -12,72 +12,121 @@ $twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
 
 $app->add(TwigMiddleware::create($app, $twig));
 
-function get_list_of_directories($path) {
+// function get_list_of_directories($path) {
+//
+// 	$iter = new RecursiveIteratorIterator(
+//     new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS),
+//     RecursiveIteratorIterator::SELF_FIRST,
+//     RecursiveIteratorIterator::CATCH_GET_CHILD // Ignore "Permission denied"
+// 	);
+//
+// 	//add the $path to the array as we need to scan for files in $path
+// 	$array_of_dirs[] = $path;
+//
+// 	//scan for folder and sub folders
+// 	$paths = array($path);
+// 	foreach ($iter as $path => $dir) {
+// 		if ($dir->isDir()) {
+// 			$array_of_dirs[] = $path;
+// 		}
+// 	}
+//
+// 	return $array_of_dirs;
+//
+// }
 
-	$iter = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS),
-    RecursiveIteratorIterator::SELF_FIRST,
-    RecursiveIteratorIterator::CATCH_GET_CHILD // Ignore "Permission denied"
-	);
+// function get_files_in_directory($directory) {
+//
+// 	$files = scandir($directory);
+//
+// 	// $output = array();
+//
+// 	// print_r($files);
+//
+// 	// foreach($file as $files) {
+// 	// 	if($file == ".") { continue; }
+// 	// 	if($file == "..") { continue; }
+// 		// print_r($files);
+// 		// array_push($output, $file);
+// 	// }
+//
+// 	return $files;
+//
+// }
 
-	//add the $path to the array as we need to scan for files in $path
-	$array_of_dirs[] = $path;
-
-	//scan for folder and sub folders
-	$paths = array($path);
-	foreach ($iter as $path => $dir) {
-		if ($dir->isDir()) {
-			$array_of_dirs[] = $path;
-		}
-	}
-
-	return $array_of_dirs;
-
+function pathToArray($path , $separator = '/') {
+    if (($pos = strpos($path, $separator)) === false) {
+        return array($path);
+    }
+    return array(substr($path, 0, $pos) => pathToArray(substr($path, $pos + 1)));
 }
 
 $app->get('/', function ($request, $response) {
 
   $view = Twig::fromRequest($request);
 
-  $dirPath = __DIR__ . '/../public/images';
+  // $dirPath = __DIR__ . '/../public/images/';
+	$dirPath = 'images/';
+	$results = array();
 
-  $array_of_directories = get_list_of_directories($dirPath);
-
-  $final_array = array();
-
-  foreach($array_of_directories as $key => $directory) {
-    // ignore the root folder
-    if ($key == 0) { continue; }
-
-    $files = scandir($directory);
-
-
-
-    foreach ($files as $file) {
-      // ignore dot file
-      if($file == ".") { continue; }
-		  if($file == "..") { continue; }
-
-      $output_array[] = array(
-        'file_name' => $file,
-      );
+	if (is_dir($dirPath)) {
+    $iterator = new RecursiveDirectoryIterator($dirPath);
+    foreach ( new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::CHILD_FIRST) as $file ) {
+        if ($file->isFile()) {
+            $thispath = str_replace('\\', '/', $file);
+            $thisfile = utf8_encode($file->getFilename());
+            $results = array_merge_recursive($results, pathToArray($thispath));
+        }
     }
+	}
 
+	// print_r($results);
 
-
-    // array_push($final_array, basename($directory);
-
-    // $final_array = array(
-    //   basename($directory) => $output_array,
-    // );
-
-  }
-
-  print_r($final_array);
+  // $array_of_directories = get_list_of_directories($dirPath);
+	//
+  // $final_array = array();
+	//
+  // foreach($array_of_directories as $key => $directory) {
+  //   // ignore the root folder
+  //   if ($key == 0) { continue; }
+	//
+	// 	// print_r($directory);
+	//
+	// 	// $files = get_files_in_directory($directory);
+	// 	$files = scandir($directory);
+	//
+	// 	// print_r($files[2]);
+	//
+  //   // $files = scandir($directory);
+	// 	//
+  //   foreach ($files as $file) {
+  //     // ignore dot file
+  //     if($file == ".") { continue; }
+	// 	  if($file == "..") { continue; }
+	//
+  //     $output_array[] = array(
+  //       'file_name' => $file,
+  //     );
+	//
+  //   }
+	//
+	// 	$files_array = array(
+  //     basename($directory) => $output_array,
+  //   );
+	//
+	// 	// print_r($output_array);
+	//
+  //   // array_push($final_array, basename($directory);
+	//
+  //   array_push($final_array, $files_array);
+	//
+  // }
+	//
+  // print_r($final_array['arbres']);
 
   return $view->render($response, 'home.html', [
       'title' => 'home',
-      // 'images' => $final_array['files']
+      'images' => $results['images']
    ]);
 })->setName('home');
 
