1db853384be0dc4ef65bc305db36d9c224decd16
jeremie nuel <jeremie.nuel@pm.me>
2024-02-19 19:11:13 +0100

    flask app

--- incomplete/__init__.py
+++ incomplete/__init__.py
@@ -1,8 +1,16 @@
+import logging
 from flask import Flask
+from werkzeug.utils import find_modules, import_string
 
 from incomplete import pages
 
+def configure_logging():
+    # register root logging
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('werkzeug').setLevel(logging.INFO)
+
 def create_app():
     app = Flask(__name__)
+    configure_logging()
     app.register_blueprint(pages.bp)
     return app
--- incomplete/pages.py
+++ incomplete/pages.py
@@ -1,11 +1,95 @@
+import os, pathlib, logging
+import glob
+from typing import List, Dict
+import pwd
+import stat
 from flask import Blueprint, render_template
 
 bp = Blueprint("pages", __name__)
+LOG = logging.getLogger(__name__)
+
+# constantes
+CURRENT_PATH = pathlib.Path().resolve()
+PATH = str(CURRENT_PATH) + '/incomplete/static/images'
+# LIST_DIR = os.listdir(PATH)
 
 @bp.route("/")
 def home():
-    return render_template("pages/home.html")
 
-@bp.route("/about")
-def about():
-    return render_template("pages/about.html")
+    # LOG.debug(directory_contents(PATH))
+
+    data = directory_contents(PATH)
+
+    return render_template("pages/home.html", data = data['data'])
+
+# ajouter la date de modification, l'extension
+
+def get_files_contents(path: str) -> List:
+    """Returns sub directory contents
+
+    Lists directory contents in a list
+
+    Args:
+        path (str): sub directory path
+    """
+
+    images = []
+    files = glob.glob(path + '/*.jpg')
+
+    for file in files:
+
+        file_attr = {}
+        file_attr['name'] = os.path.basename(file)
+        # size in kilobytes
+        file_attr['size'] = '{} kb'.format(round(os.path.getsize(file) / 1028))
+        if os.path.isfile(file):
+            file_attr['type'] = 'file'
+        else:
+            file_attr['type'] = 'dir'
+        images.append(file_attr)
+
+    return images
+
+def directory_contents(path: str) -> Dict:
+    """Returns root directory contents
+
+    Lists directory contents in a dict
+
+    Args:
+        path (str): directory path
+    """
+    root_files = { 'data' : [] }
+    directory = os.listdir(path)
+    # print(files)
+    for dir in directory:
+
+        file_attr = {}
+        file_path = '{}/{}'.format(path, dir)
+        file_attr['name'] = os.path.basename(file_path)
+        # size in kilobytes
+        file_attr['size'] = '{} kb'.format(round(os.path.getsize(file_path) / 1028))
+        file_attr['files'] = get_files_contents(file_path)
+        if os.path.isfile(file_path):
+            file_attr['type'] = 'file'
+        else:
+            file_attr['type'] = 'dir'
+        root_files['data'].append(file_attr)
+    # bd.logger.debug(json_body)
+    return root_files
+
+def get_owner(path):
+    """Get owner of file or directory
+
+    Args:
+        path (str): file path
+    """
+    stat = os.stat(path)
+    return pwd.getpwuid(stat.st_uid)[0]
+
+def get_permissions(path):
+    """Get permission of file or directory in octal
+
+    Args:
+        path (str): file path
+    """
+    return oct(stat.S_IMODE(os.stat(path).st_mode))[-3:]
