dd458a8f1ebeff82d37f2b667770347006d3fe8a
jeremie nuel <jeremie.nuel@pm.me>
2024-02-19 15:49:59 +0100

    tableaux

---
+++ VERSIONS.md
@@ -0,0 +1,32 @@
+0.1 à 0.6
+construction d'un système basé sur une application flask (python)
+
+0.7
+changement d'organisation, suppression des dossiers
+tentative de tri par densité de noir
+corrections du contraste
+
+0.8
+retour à l'organisation par dossiers
+chapitrage par mots-clef
+nouveau principe de couverture
+
+0.9
+ajout d'un pseudonyme comme auteur
+reconstruction à partir de https://gitlab.coko.foundation/pagedjs/pagedjs-book-template
+
+1.0
+script node.js pour générer l'html et le servir
+Ajout d'une citation de Mayem en introduction
+Le texte à la fin
+
+1.1
+calcul de la densité noir et blanc des images pour les organiser
+nouvelle couverture *blow blow thou winter wind*
+
+1.2
+caractères Monospace 821BT dessiné par Max Medinger et Baskervville de l'ANRT
+Mise à jour des images
+
+2.0
+page web qui génère le livre
--- public/index.php
+++ public/index.php
@@ -12,48 +12,7 @@ $twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
 
 $app->add(TwigMiddleware::create($app, $twig));
 
-// function get_list_of_directories($path) {
-//
-// 	$iter = new RecursiveIteratorIterator(
-//     new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS),
-//     RecursiveIteratorIterator::SELF_FIRST,
-//     RecursiveIteratorIterator::CATCH_GET_CHILD // Ignore "Permission denied"
-// 	);
-//
-// 	//add the $path to the array as we need to scan for files in $path
-// 	$array_of_dirs[] = $path;
-//
-// 	//scan for folder and sub folders
-// 	$paths = array($path);
-// 	foreach ($iter as $path => $dir) {
-// 		if ($dir->isDir()) {
-// 			$array_of_dirs[] = $path;
-// 		}
-// 	}
-//
-// 	return $array_of_dirs;
-//
-// }
-
-// function get_files_in_directory($directory) {
-//
-// 	$files = scandir($directory);
-//
-// 	// $output = array();
-//
-// 	// print_r($files);
-//
-// 	// foreach($file as $files) {
-// 	// 	if($file == ".") { continue; }
-// 	// 	if($file == "..") { continue; }
-// 		// print_r($files);
-// 		// array_push($output, $file);
-// 	// }
-//
-// 	return $files;
-//
-// }
-
+/* fonction recusive pour écrire le nom des dossiers et des fichiers */
 function pathToArray($path , $separator = '/') {
     if (($pos = strpos($path, $separator)) === false) {
         return array($path);
@@ -61,68 +20,33 @@ function pathToArray($path , $separator = '/') {
     return array(substr($path, 0, $pos) => pathToArray(substr($path, $pos + 1)));
 }
 
+
 $app->get('/', function ($request, $response) {
 
   $view = Twig::fromRequest($request);
 
-  // $dirPath = __DIR__ . '/../public/images/';
 	$dirPath = 'images/';
 	$results = array();
 
+  /* fonction pour parcourir le système de fichier
+  et fabriquer un tableau à plusieurs dimensions
+  avec le nom des dossiers et fichiers */
 	if (is_dir($dirPath)) {
     $iterator = new RecursiveDirectoryIterator($dirPath);
     foreach ( new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::CHILD_FIRST) as $file ) {
         if ($file->isFile()) {
             $thispath = str_replace('\\', '/', $file);
-            $thisfile = utf8_encode($file->getFilename());
             $results = array_merge_recursive($results, pathToArray($thispath));
         }
     }
 	}
 
-	// print_r($results);
-
-  // $array_of_directories = get_list_of_directories($dirPath);
-	//
-  // $final_array = array();
-	//
-  // foreach($array_of_directories as $key => $directory) {
-  //   // ignore the root folder
-  //   if ($key == 0) { continue; }
-	//
-	// 	// print_r($directory);
-	//
-	// 	// $files = get_files_in_directory($directory);
-	// 	$files = scandir($directory);
-	//
-	// 	// print_r($files[2]);
-	//
-  //   // $files = scandir($directory);
-	// 	//
-  //   foreach ($files as $file) {
-  //     // ignore dot file
-  //     if($file == ".") { continue; }
-	// 	  if($file == "..") { continue; }
-	//
-  //     $output_array[] = array(
-  //       'file_name' => $file,
-  //     );
-	//
+  // foreach ($results as $result) {
+  //   print_r($result);
+  //   foreach ($result as $image) {
+  //     print_r($image);
   //   }
-	//
-	// 	$files_array = array(
-  //     basename($directory) => $output_array,
-  //   );
-	//
-	// 	// print_r($output_array);
-	//
-  //   // array_push($final_array, basename($directory);
-	//
-  //   array_push($final_array, $files_array);
-	//
   // }
-	//
-  // print_r($final_array['arbres']);
 
   return $view->render($response, 'home.html', [
       'title' => 'home',
