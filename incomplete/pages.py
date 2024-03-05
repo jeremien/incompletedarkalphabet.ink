adc0fe36d878b9f94f32e036d7c91c9d489b066f
Jérémie Nuel <jeremie.nuel@pm.me>
2024-03-05 15:27:19 +0100

    les iamges sont converti en webp pour gagner de la place

--- incomplete/pages.py
+++ incomplete/pages.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from typing import List, Dict
 import pwd
 import stat
+import pathlib
 from flask import Blueprint, render_template
 
 bp = Blueprint("pages", __name__, template_folder='templates')
@@ -32,7 +33,7 @@ def home():
     total_mb = round((total_bytes / 1024) / 1024)
     total_files = get_num_files(PATH)
 
-    total_data = { 'mb': total_mb, 'files': total_files }
+    total_data = { 'mb': total_mb, 'files': total_files, 'type': '.webp', 'size': '850x1390' }
     #LOG.debug(total_files)
 
     return render_template("pages/home.html",
@@ -75,14 +76,15 @@ def get_files_contents(path: str) -> List:
     """
 
     images = []
-    files = glob.glob(path + '/*.jpg')
+    files = glob.glob(path + '/*.webp')
 
     for file in files:
 
         file_attr = {
             'name': os.path.basename(file),
             'date': os.path.getmtime(file),
-            'size': '{}'.format(round(os.path.getsize(file) / 1024))
+            'size': '{}'.format(round(os.path.getsize(file) / 1024)),
+            'type': os.path.splitext(file),
         }
         # size in kilobytes
         if os.path.isfile(file):
